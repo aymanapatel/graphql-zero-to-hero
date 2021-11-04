b1e9786eca3affbb9007904cb67b00c9c035404e
Ayman <ayman.patel97@gmail.com>
2021-11-04 16:39:18 +0530

    feat: #19 GraphQLServletListener

---
+++ src/main/java/com/ayman/learngraphqljava/listener/LoggingListener.java
@@ -0,0 +1,45 @@
+package com.ayman.learngraphqljava.listener;
+
+import graphql.kickstart.servlet.core.GraphQLServletListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LoggingListener implements GraphQLServletListener {
+
+
+    private final Clock clock;
+
+    @Override
+    public RequestCallback onRequest(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Start: GraphQL Request started");
+        var startTime = Instant.now(clock);
+        return new RequestCallback() {
+            @Override
+            public void onSuccess(HttpServletRequest request, HttpServletResponse response) {
+                //no-op
+            }
+
+            @Override
+            public void onError(HttpServletRequest request, HttpServletResponse response, Throwable throwable) {
+                //no-op
+            }
+
+            @Override
+            public void onFinally(HttpServletRequest request, HttpServletResponse response) {
+                log.info("Completed Request: Time Taken {} ", Duration.between(startTime, Instant.now(clock)));
+            }
+        };
+    }
+
+
+}
