330f96bc2b282e2fd5c740390d1750c1b985fbd1
Ayman <ayman.patel97@gmail.com>
2021-11-04 18:03:53 +0530

    feat: #21 Pagination

--- src/main/java/com/ayman/learngraphqljava/resolver/bank/query/BankAccountResolver.java
+++ src/main/java/com/ayman/learngraphqljava/resolver/bank/query/BankAccountResolver.java
@@ -1,19 +1,30 @@
 package com.ayman.learngraphqljava.resolver.bank.query;
 
+import com.ayman.learngraphqljava.connection.CursorUtil;
 import com.ayman.learngraphqljava.domain.bank.BankAccount;
 import com.ayman.learngraphqljava.domain.bank.Client;
 import com.ayman.learngraphqljava.domain.bank.Currency;
+import com.ayman.learngraphqljava.repository.BankAccountRepository;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.relay.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class BankAccountResolver implements GraphQLQueryResolver {
+
+    private final BankAccountRepository bankAccountRepository;
+    private final CursorUtil cursorUtil;
 
     public BankAccount bankAccount(UUID id) {
         log.info("Retrieving bank account id: {}", id);
@@ -23,8 +34,32 @@ public class BankAccountResolver implements GraphQLQueryResolver {
 //        clientB.setClient(clientA);
 
         return BankAccount.builder().id(id).currency(Currency.INR).build();
+    }
 
 
+    // Pagination
+    public Connection<BankAccount> bankAccounts(int first, @Nullable String cursor) {
+        List<Edge<BankAccount>> edges = getBankAccounts(cursor)
+                .stream()
+                .map(bankAccount -> new DefaultEdge<>(bankAccount,
+                        cursorUtil.createCursorWith(bankAccount.getId())))
+                .limit(first)
+                .collect(Collectors.toUnmodifiableList());
+
+        var pageInfo = new DefaultPageInfo(
+                cursorUtil.getFirstCursorFrom(edges),
+                cursorUtil.getLastCursorFrom(edges),
+                cursor != null,
+                edges.size() >= first);
+
+        return new DefaultConnection<>(edges, pageInfo);
+    }
+
+    public List<BankAccount> getBankAccounts(String cursor) {
+        if (cursor == null) {
+            return bankAccountRepository.getBankAccounts();
+        }
+        return bankAccountRepository.getBankAccountsAfter(cursorUtil.decode(cursor));
     }
 
 }
