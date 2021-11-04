83ba19f6f535a3e112787d245c5ec0af4e261a62
Ayman <ayman.patel97@gmail.com>
2021-11-04 13:02:05 +0530

    feat: #7 DataFetchResult for data and error response

--- src/main/java/com/ayman/learngraphqljava/resolver/ClientResolver.java
+++ src/main/java/com/ayman/learngraphqljava/resolver/ClientResolver.java
@@ -2,18 +2,41 @@ package com.ayman.learngraphqljava.resolver;
 
 import com.ayman.learngraphqljava.domain.bank.BankAccount;
 import com.ayman.learngraphqljava.domain.bank.Client;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.UUID;
 
 @Component
 @Slf4j
 public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public Client client(BankAccount bankAccount) {
-        log.info("Requesting client data for bank account id {} ", bankAccount.getId());
-        return Client.builder().firstName("John").middleName(List.of("Cox")).lastName("Stewart").build();
+
+    /**
+     * Simple Resolver
+     * public Client client(BankAccount bankAccount) {
+     *         log.info("Requesting client data for bank account id {} ", bankAccount.getId());
+     *         return Client.builder().firstName("John").middleName(List.of("Cox")).lastName("Stewart").build();
+     *     }
+     */
+
+    /**
+     * Complex DataFetchResult resolver
+     * 1. Call multiple services
+     * 2. Call another graphql server
+     * 3. Call service that returns partial response
+     */
+    public DataFetcherResult<Client> client(BankAccount bankAccount) {
+        log.info("Requesting client data for bank account id {} (mutiple services)", bankAccount.getId());
+
+        return DataFetcherResult.<Client>newResult()
+                    .data(Client.builder().id(UUID.randomUUID()).firstName("John").lastName("Stewart").build())
+                    .error(new GenericGraphQLError("Cannot retrieve client id"))
+                    .build();
+
     }
 }
