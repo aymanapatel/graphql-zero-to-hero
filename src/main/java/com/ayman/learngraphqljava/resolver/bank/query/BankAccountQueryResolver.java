9d79c7a702647fe932daba7a51e7b0e471d2968c
Ayman <ayman.patel97@gmail.com>
2021-11-05 17:55:15 +0530

    fix: Refactor BankAccountResolver.java to BankAccountQueryResolver

--- src/main/java/com/ayman/learngraphqljava/resolver/bank/query/BankAccountQueryResolver.java
+++ src/main/java/com/ayman/learngraphqljava/resolver/bank/query/BankAccountQueryResolver.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class BankAccountResolver implements GraphQLQueryResolver {
+public class BankAccountQueryResolver implements GraphQLQueryResolver {
 
     private final BankAccountRepository bankAccountRepository;
     private final CursorUtil cursorUtil;
